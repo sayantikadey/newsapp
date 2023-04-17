1e5a18e1fdb40abae743e00d2948fd6544cee8a6
Sayantika Dey <Sayantikasuri@gmail.com>
2023-04-17 09:30:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function App() {
+  const [feed, setFeed] = useState(null);
+  const flatlist = () => {
+    axios({
+      method: "get",
+      url: `https://newsapi.org/v2/top-headlines?country=in&apiKey=69f898a10cfc48e4b44030c485e12bf0`,
+    })
+      .then((response) => setFeed(response.data.articles))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    flatlist();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ width: "100%" }}>
+        <FlatList
+          data={feed}
+          keyExtractor={(item, index) => item.url}
+          renderItem={({ item, index }) => <Text>{item.content}</Text>}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
